9d2e29531fd05fa7eb9f935f5c2596ee4b554506
brittanieb <brittanie.boyko@gmail.com>
2019-12-09 23:56:43 -0500

    created questions.js and added array of question objects

---
+++ resources/js/questions.js
@@ -0,0 +1,24 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "This is a third question.",
+        choices: ["anwer one", "answer two", "answer three", "answer four"],
+        answer: "answer one"
+    },
+    {
+        title: "This is question number four",
+        choices: ["anwer one", "answer two", "answer three", "answer four"],
+        answer: "answer one"
+    },
+    ///etc.
+  ];
+  
