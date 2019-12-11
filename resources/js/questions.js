5d5cd2e87ae25df4f4764ac14ddb62989f544a23
brittanieb <brittanie.boyko@gmail.com>
2019-12-11 18:31:34 -0500

    added more questions and answers to questions.js

--- resources/js/questions.js
+++ resources/js/questions.js
@@ -10,14 +10,25 @@ var questions = [
         answer: "parentheses"
     },
     {
-        title: "This is a third question.",
-        choices: ["anwser one", "answer two", "answer three", "answer four"],
-        answer: "answer one"
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<scripting> ", "<js>", "<script>", "<javascript>"],
+        answer: "<script>"
     },
     {
-        title: "This is question number four",
-        choices: ["answer one", "answer two", "answer three", "answer four"],
-        answer: "answer one"
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["the <head> section", "the <body> section", "Both the <head> and the <body> section are correct", "None of the above"],
+        answer: "the <body> section"
     },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function = myFunction()", "function:myFunction()", "function myFunction()"],
+        answer: "function myFunction()"
+    },
+    {
+        title: "How does a FOR loop start?",
+        choices: ["for (i <= 5; i++)", "for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5"],
+        answer: "for (i = 0; i <= 5; i++)"
+    },
+    
   ];
   
